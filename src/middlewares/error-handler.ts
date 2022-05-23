f49c00929f250cd0af36fc5cc712c169c9c7a41d
george <calin.popescu@gdm.ro>
2022-05-23 10:21:48 +0300

    Updates

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,23 @@
+import { Request, NextFunction, Response } from "express";
+import { CustomError } from "../errors/custom-error";
+
+export const errorHandler = (
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof CustomError) {
+    return res.status(error.statusCode).send({
+      errors: error.serializeErrors(),
+    });
+  }
+
+  res.status(400).send({
+    error: [
+      {
+        message: "Something went wrong",
+      },
+    ],
+  });
+};
